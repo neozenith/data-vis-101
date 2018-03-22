ce2e834f5e234e81dfcc6265fed050425007e77f
Josh Peak <neozenith.dev@gmail.com>
2018-03-23 10:53:07 +1100

    use const instead of var

--- dist/js/plot.js
+++ dist/js/plot.js
@@ -1,15 +1,15 @@
 'use strict';
 
-console.log('BULLSHIT');
+console.log('plot.js');
 
-var margin = { top: 20, right: 30, bottom: 50, left: 40 },
+const margin = { top: 20, right: 30, bottom: 50, left: 40 },
 	width = 960 - margin.left - margin.right,
 	height = 500 - margin.top - margin.bottom;
 
 // Define a linear scale for output geometry
 
 // append the svg object to the body of the page
-var svg = d3
+const svg = d3
 	.select('#chart')
 	.append('svg')
 	.attr('width', width + margin.left + margin.right)
