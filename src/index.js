8a9275de1654566f2374fff4a004ef2bd794a290
Josh Peak <neozenith.dev@gmail.com>
2018-04-09 14:22:09 +1000

    Revert to single file implementation for ease of demonstration for upcoming talk

--- src/index.js
+++ src/index.js
@@ -18,7 +18,96 @@
 //
 // build:prod:: 71kb
 
-// TODO: Refactor this
+const d3 = Object.assign(
+	{},
+	require('d3-selection'),
+	require('d3-scale'),
+	require('d3-axis'),
+	require('d3-format'),
+	require('d3-time-format'),
+	require('d3-dsv'),
+	require('d3-array'),
+	require('d3-fetch')
+);
+
+const margin = { top: 50, right: 50, bottom: 50, left: 50 },
+	width = 960,
+	height = 500;
+
+const chartArea = {
+	x: margin.left,
+	y: margin.top,
+	width: width - margin.left - margin.right,
+	height: height - margin.top - margin.bottom
+};
+
+// --------------- SCALES ---------------
+// Define a linear scale for output geometry
+const scales = [];
+// Output pixel range of the chartArea <g> grouping. The <g> will be translated by the margin offset
+const xRange = [0, chartArea.width];
+// Y-Range starts lower down screen then progresses up towards zero for larger domain values
+const yRange = [chartArea.height, 0];
+scales.push(d3.scaleTime().range(xRange));
+scales.push(d3.scaleLinear().range(yRange));
+
+// --------------- CHART  ---------------
+const svg = d3
+	.select('#chart')
+	.append('svg')
+	.attr('viewBox', '0 0 ' + width + ' ' + height);
+
+const svgChartArea = svg
+	.append('g')
+	.attr('id', 'chartArea')
+	.attr('transform', 'translate(' + chartArea.x + ',' + chartArea.y + ')');
+
+// Axes constructor objects
+// TODO: Label axes
+const axes = [];
+axes.push(d3.axisBottom(scales[0]).tickFormat(d3.timeFormat("%b'%y")));
+axes.push(d3.axisLeft(scales[1]).tickFormat(d3.format('$0.2f')));
+
+// --------------- AXES  ---------------
+// SVG Selection for Axes
+const svgAxes = [];
+svgAxes.push(
+	svg
+		.append('g')
+		.call(axes[0])
+		.attr('transform', `translate(${chartArea.x}, ${chartArea.height + chartArea.y})`)
+);
+svgAxes.push(
+	svg
+		.append('g')
+		.call(axes[1])
+		.attr('transform', `translate(${chartArea.x}, ${chartArea.y})`)
+);
+
+// --------------- DATA  ---------------
+// Load data, parse it, render it
+
+/**
+ * asynccreateChart() render chart and parse data
+ *
+ * @param {String} csvFile - path to source csv data file
+ * @param {Function} rowParser - function for parsing each row of csv data
+ * @param {Function} graphBuilder - function used to draw graph after dataset loaded
+ *
+ * @return {void} Return nothing
+ */
+async function createChart(csvFile, rowParser, graphBuilder) {
+	const dataset = await d3.csv(csvFile, parseRow).catch(err => console.log(err));
+	graphBuilder(dataset);
+}
+
+/**
+ * parseRow() Parsing callback function for coercing data types from CSV
+ *
+ * @param {Object} data - raw parsed data object from D3 where all fields are strings
+ *
+ * @return {Object} fields rewritten with correctly coerced data types
+ */
 function parseRow(data) {
 	const row = {};
 
@@ -50,12 +139,20 @@ function parseRow(data) {
 
 	return row;
 }
+
+/**
+ * renderGraph() Callback function to operate on returned dataset from D3 and update elements
+ *
+ * @param {Array} data - Array of Objects parsed from CSV
+ *
+ * @return {void}
+ */
 function renderGraph(data) {
 	const keys = ['Date', 'Cost ($)', 'Litres', 'Distance'];
 	console.log('postParse');
 	console.log(data);
 
-	for (let i = 0; i < 3; i++) {
+	for (let i = 0; i < scales.length; i++) {
 		scales[i].domain([
 			d3.min(data, d => {
 				return d[keys[i]];
@@ -77,7 +174,8 @@ function renderGraph(data) {
 		.enter()
 		.append('circle')
 		.attr('r', 3)
-		.attr('fill', '#df8787')
+		.attr('stroke', '#8787df')
+		.attr('fill', 'none')
 		.attr('cx', function(d) {
 			return scales[0](d[keys[0]]);
 		})
@@ -89,28 +187,6 @@ function renderGraph(data) {
 		})
 		.attr('data-y', function(d) {
 			return d[keys[1]];
-		});
-
-	// Series 2
-	svgChartArea
-		.selectAll('rect')
-		.data(data)
-		.enter()
-		.append('rect')
-		.attr('width', 6)
-		.attr('height', 6)
-		.attr('fill', '#8787df')
-		.attr('x', function(d) {
-			return scales[0](d[keys[0]]) - 3;
-		})
-		.attr('y', function(d) {
-			return scales[2](d[keys[2]]) - 3;
-		})
-		.attr('data-x', function(d) {
-			return d[keys[0]];
-		})
-		.attr('data-y', function(d) {
-			return d[keys[2]];
 		});
 }
 
