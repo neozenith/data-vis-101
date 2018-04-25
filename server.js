7bc8933208e67f51ad70b1a8a8770accd49da374
Josh Peak <neozenith.dev@gmail.com>
2018-04-26 08:01:42 +1000

    Add Webpack dev middleware to improve dev cycle time

--- server.js
+++ server.js
@@ -5,6 +5,7 @@ const express = require('express'),
 	path = require('path'),
 	compression = require('compression'),
 	bodyParser = require('body-parser'),
+	morgan = require('morgan'),
 	api = require('./api');
 
 // Config
@@ -12,19 +13,37 @@ const pkg = require('./package.json');
 
 // Server Setup
 const port = process.env.PORT || 3000;
-const environment = process.env.NODE_ENV || 'DEVELOPMENT';
+const environment = process.env.NODE_ENV || 'development';
 const staticPath = process.env.STATIC_PATH || path.join(__dirname, 'dist');
 const app = express();
+app.use(morgan('dev'));
 app.use(compression());
 app.use(bodyParser.json());
 
 console.log(`${environment} v${pkg.version}`);
 
-// Static Assets
-app.use(express.static(staticPath));
+/*============================== STATIC ASSETS ============================== */
 
+if (environment === 'development') {
+	const webpack = require('webpack');
+	const webpackMiddleware = require('webpack-dev-middleware');
+	const webpackConfig = require('./webpack.config.js');
+	webpackConfig.mode = environment;
+
+	app.use(
+		webpackMiddleware(webpack(webpackConfig), {
+			publicPath: '/',
+			stats: { colors: true }
+		})
+	);
+} else {
+	app.use(express.static(staticPath));
+}
+
+/*============================== ROUTES============================== */
 app.post('/api/arduino', api.arduino);
 
+/*============================== ERROR HANDLING ============================== */
 /* eslint-disable no-unused-vars */
 // 404
 app.use(function(req, res, next) {
@@ -40,3 +59,6 @@ app.use(function(err, req, res, next) {
 
 // Serving
 app.listen(port, () => console.log(`http://localhost:${port}`));
+
+// Export for Integration testing
+module.exports = app;
