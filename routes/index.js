2a1fef74288c988501f9b31f0c7d24fe9169e693
Josh Peak <neozenith.dev@gmail.com>
2018-06-08 17:01:05 +1000

    refactor collection initialisation

--- routes/index.js
+++ routes/index.js
@@ -4,7 +4,12 @@ const express = require('express');
 const router = express.Router();
 const { initMongoRoute } = require('./mongo-route-template');
 
-initMongoRoute(router, 'sensor');
-// initMongoRoute(router, 'anothercollection');
+// Define named list of collections
+const collections = ['sensor'];
+
+//Iterate through initialising them
+collections.forEach(c => {
+  initMongoRoute(router, c);
+});
 
 module.exports = router;
